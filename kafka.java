2f8383c9ff88bc02d7e21bf69cc416d6521beaef
Prathibha Jajav <jajavprathibha@gmail.com>
2026-03-06 16:46:29 -0600

    Add JSON data for student records

---
+++ kafka.java
@@ -0,0 +1,37 @@
+{
+  "students":[
+  {
+        "id": 1,
+        "name": "John Doe",
+        "age": 18,
+        "grade": "12th",
+        "subjects": [
+            "Math",
+            "Physics",
+            "English"
+        ]
+    },
+    {
+        "id": 2,
+        "name": "Jane Smith",
+        "age": 17,
+        "grade": "11th",
+        "subjects": [
+            "Biology",
+            "Chemistry",
+            "History"
+        ]
+    },
+    {
+        "id": 3,
+        "name": "David Johnson",
+        "age": 16,
+        "grade": "10th",
+        "subjects": [
+            "Computer Science",
+            "Spanish",
+            "Art"
+        ]
+       }
+    ]
+}
